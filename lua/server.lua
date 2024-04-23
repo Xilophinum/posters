16124c7d11fd7aa223f92c618441177640e8ba85
ItsMuri <88394932+ItzMuri@users.noreply.github.com>
2024-04-23 14:45:57 +0200

    qb inventory support

--- lua/server.lua
+++ lua/server.lua
@@ -6,9 +6,12 @@ QBCore.Functions.CreateUseableItem('poster', function(source, item)
 end)
 
 RegisterNetEvent("posters:addNewImage", function(data)
+    local Player = QBCore.Functions.GetPlayer(source)
     RegisteredSprays[#RegisteredSprays+1] = data
     TriggerClientEvent("posters:sendAddedImage", -1, data)
-    exports.ox_inventory:RemoveItem(source, "poster", 1)
+    --exports.ox_inventory:RemoveItem(source, "poster", 1)
+    if not Player then return end
+    Player.Functions.RemoveItem("poster", 1)
 end)
 
 lib.callback.register('posters:getImages', function(source)
@@ -16,6 +19,7 @@ lib.callback.register('posters:getImages', function(source)
 end)
 
 RegisterNetEvent("posters:deleteImage", function(id, isOwner)
+    local Player = QBCore.Functions.GetPlayer(source)
     for k,v in pairs(RegisteredSprays) do
         if v.id == id then
             table.remove(RegisteredSprays, k)
@@ -23,9 +27,12 @@ RegisterNetEvent("posters:deleteImage", function(id, isOwner)
         end
     end
     if isOwner then
-        exports.ox_inventory:AddItem(source, "poster", 1)
+        --exports.ox_inventory:AddItem(source, "poster", 1)
+        if not Player then return end
+        Player.Functions.AddItem("poster", 1)
     end
 end)
+
 
 RegisterCommand("removeposter", function(source, args, raw)
     TriggerClientEvent("posters:removePoster", source)
